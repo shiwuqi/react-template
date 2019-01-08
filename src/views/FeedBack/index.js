ba19acf30aa0b2c289c686aa816bdbefaa5caf0c
Zi.Li <lz123456>
2019-01-08 19:25:24 +0800

    react生命周期

--- src/views/FeedBack/index.js
+++ src/views/FeedBack/index.js
@@ -12,8 +12,57 @@ class FeedBack extends React.Component {
     }
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // 组件实例化后和接受新的props后被调用（包括父组件的props改变）
+    // 调用setState()不会触发该方法
+    console.log('getDerivedStateFromProps', nextProps, prevState)
+    return null
+  }
+
+  // componentWillMount() {
+  //   // 将被废弃
+  //   // 服务器渲染的唯一方法
+  // }
+
   componentDidMount () {
     this.querySystemFeedBack()
+  }
+
+  
+  // componentWillReceiveProps(nextProps) {
+  //   // 将被废弃
+  //   // props被父组件更改，使用setData()，以触发render方法重新渲染组件
+  // }
+
+  shouldComponentUpdate() {
+    return true
+    // true重新render false不会重写render 默认true
+    // 当前的状态或属性的改变是否会影响组件的输出
+  }
+
+  // componentWillUpdate(nextProps, nextState) {
+  //   // 将被废弃
+       // 在渲染新的state或props时被调用
+       // 不能使用setData()，不能做会触发视图更新的操作
+  // }
+
+  getSnapshotBeforeUpdate() {
+    // 在render后的输出被渲染到DOM之前被调用
+    // 它使您的组件能够在它们被潜在更改之前捕获当前值（如滚动位置）
+    // 该生命周期返回的任何值都将作为参数传递给componentDidUpdate（）
+    return null
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // 渲染完成后调用一次，这个时候DOM已经渲染了
+  }
+
+  componentWillUnmount() {
+    // 组件被卸载并销毁之前被调用
+  }
+
+  componentDidCatch(error, info) {
+    // 错误边界只会捕获树中下面组件中的错误，错误边界本身不能捕获错误
   }
 
   querySystemFeedBack = async (params) => {
