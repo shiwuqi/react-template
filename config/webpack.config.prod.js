5470b7cea5fb5d98767ede0f4be52b612e8ce4e7
十七 <lizi@192.168.0.103>
2019-05-25 21:54:31 +0800

    less-loader优化

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -97,6 +97,7 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
       loader: require.resolve(preProcessor),
       options: {
         sourceMap: shouldUseSourceMap,
+        javascriptEnabled: true
       },
     });
   }
